bf3c65a4ad0b779866dbc8ef2e7ff5986f8c6f49
abbas <kareemabbas106@gmail.com>
2020-10-07 21:03:20 +0200

    Creating the form for submit the tweet

--- tweetme2/tweets/views.py
+++ tweetme2/tweets/views.py
@@ -2,13 +2,24 @@ import random
 from django.http import Http404, JsonResponse
 from django.shortcuts import render
 
+from .forms import TweetForm
 from .models import Tweet
 
 
-
+# urls('')
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
+# urls('create-tweet')
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save ()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})
+
+# urls('tweet/')
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
     tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 100)} for x in qs]
@@ -18,6 +29,7 @@ def tweet_list_view(request, *args, **kwargs):
     }
     return JsonResponse(data)
 
+# urls('tweet/<int:tweet_id>')
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     data = {
         "id": tweet_id,
