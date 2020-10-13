287f0a2b201b006fa8d53f1c4cd50fff9a3f3cdb
abbas <kareemabbas106@gmail.com>
2020-10-13 21:52:59 +0200

    33 Users& Tweets

--- tweetme2/tweets/models.py
+++ tweetme2/tweets/models.py
@@ -1,7 +1,13 @@
-from django.db import models
 import random
+from django.conf import settings
+from django.db import models
+
+User = settings.AUTH_USER_MODEL
+
+# superuser passowrd: 0000123400, username: abbas, email: blank
 class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #  many user can have many tweets
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
     class Meta:
